5cfce75c87bf8a7420f4b1af67db435645a56b5a
maryandrioli <maryandrioli@gmail.com>
2024-12-03 19:19:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
 const CACHE_NAME = 'sons-sr-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/sons/1.wav',
-  '/sons/2.wav',
-  '/sons/3.wav',
-  '/sons/4.wav',
-  '/sons/5.wav',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './sons/1.wav',
+  './sons/2.wav',
+  './sons/3.wav',
+  './sons/4.wav',
+  './sons/5.wav',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
@@ -23,6 +24,16 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
   );
 });
